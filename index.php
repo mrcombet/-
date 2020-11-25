a01ede2e1579b976575f6a6d907ae84b1b546827
whoami <74049620+whoami-45@users.noreply.github.com>
2020-11-26 00:29:52 +0800

    nuklir reader

---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Nuklir Reader</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+<link rel="stylesheet" href="../style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div id="Gallery">
+  <div class="gallery-item">
+    <a href="1.jpg"><img src="1.jpg"/></a>
+  </div>
+  <div class="gallery-item">
+    <a href="2.jpg"><img src="2.jpg"/></a>
+  </div>
+  <div class="gallery-item">
+    <a href="3.jpg"><img src="3.jpg"/></a>
+  </div>
+  <div class="gallery-item">
+    <a href="4.jpg"><img src="4.jpg"/></a>
+  </div>
+  <div class="gallery-item">
+    <a href="5.jpg"><img src="5.jpg"/></a>
+  </div>
+  <div class="gallery-item">
+    <a href="6.jpg"><img src="6.jpg"/></a>
+  </div>
+  <div class="gallery-item">
+    <a href="7.jpg"><img src="7.jpg"/></a>
+  </div>
+  <div class="gallery-item">
+    <a href="8.jpg"><img src="8.jpg"/></a>
+  </div>
+  <div class="gallery-item">
+    <a href="9.jpg"><img src="9.jpg"/></a>
+  </div>
+  <div class="gallery-item">
+    <a href="10.jpg"><img src="10.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="11.jpg"><img src="11.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="12.jpg"><img src="12.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="13.jpg"><img src="13.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="14.jpg"><img src="14.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="15.jpg"><img src="15.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="16.jpg"><img src="16.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="17.jpg"><img src="17.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="18.jpg"><img src="18.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="19.jpg"><img src="19.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="20.jpg"><img src="20.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="21.jpg"><img src="21.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="22.jpg"><img src="22.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="23.jpg"><img src="23.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="24.jpg"><img src="24.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="25.jpg"><img src="25.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="26.jpg"><img src="26.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="27.jpg"><img src="27.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="28.jpg"><img src="28.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="29.jpg"><img src="29.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="30.jpg"><img src="30.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="31.jpg"><img src="31.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="32.jpg"><img src="32.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="33.jpg"><img src="33.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="34.jpg"><img src="34.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="35.jpg"><img src="35.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="36.jpg"><img src="36.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="37.jpg"><img src="37.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="38.jpg"><img src="38.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="39.jpg"><img src="39.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="40.jpg"><img src="40.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="41.jpg"><img src="41.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="42.jpg"><img src="42.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="43.jpg"><img src="43.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="44.jpg"><img src="44.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="45.jpg"><img src="45.jpg"/></a>
+  </div>
+    <div class="gallery-item">
+    <a href="46.jpg"><img src="46.jpg"/></a>
+  </div>
+    </a>
+  </div>
+</div>
+<!--end #Gallery-->
+<!-- partial -->
+  <script src='https://abvichico.com/js/simple-inheritance.min.js'></script>
+<script src='https://abvichico.com/js/code-photoswipe-1.0.11.min.js'></script><script  src="../script.js"></script>
+<script type="text/javascript">
+        document.addEventListener("DOMContentLoaded", function(event) {
+   document.querySelectorAll('img').forEach(function(img){
+  	img.onerror = function(){this.style.display='none';};
+   })
+});
+    </script>
+
+</body>
+</html>
